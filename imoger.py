7ec161ba8c9b03cae80d01dbf980f36f63fd286a
Matthias Gilch <matthias.gilch.mg@gmail.com>
2021-04-09 15:42:02 +0200

    Copying and directory structure

--- imoger.py
+++ imoger.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import pathlib
 import datetime
+import shutil
 
 try:
     from exif import Image
 except ImportError:
     import sys
     sys.stderr.write('exif not available. Install with ')
+    exit(1)
 
 
-def main(search: pathlib.Path, target: pathlib.Path, recursive=True, print_progress=False, verbose=False):
+def main(search: pathlib.Path, target: pathlib.Path,
+         recursive=True, print_progress=False,
+         date_dirs=False,
+         move=False, verbose=False):
     search_string = None
     if recursive:
         search_string = '**/*.JPG'
@@ -53,10 +58,22 @@ def main(search: pathlib.Path, target: pathlib.Path, recursive=True, print_progr
                          f'{date.hour:02}{date.minute:02}{date.second:02}',
                          f'{i+1:02}.JPG')
             file_name = '_'.join(file_name)
+
+            new_path = target
+            if date_dirs:
+                new_path = new_path / \
+                           f'{date.year:04}' / \
+                           f'{date.month:02}' / \
+                           f'{date.day:02}'
+                if not new_path.exists():
+                    new_path.mkdir(parents=True)
+            new_path = new_path / file_name
             if verbose:
-                print(f'Copy {path} -> {file_name}')
-            new_path = target / file_name
-            path.rename(new_path)
+                print(f'Copy {path} -> {new_path}')
+            if move:
+                path.rename(new_path)
+            else:
+                shutil.copyfile(path, new_path)
 
 
 if __name__ == '__main__':
@@ -64,14 +81,23 @@ if __name__ == '__main__':
     prsr = argparse.ArgumentParser()
     prsr.add_argument('search')
     prsr.add_argument('target')
-    prsr.add_argument('-r', '--recursive', action='store_true')
-    prsr.add_argument('-p', '--progress', action='store_true')
-    prsr.add_argument('-v', '--verbose', action='store_true')
+    prsr.add_argument('-r', '--recursive', action='store_true',
+                      help='Search recursive')
+    prsr.add_argument('-p', '--progress', action='store_true',
+                      help='Show progress')
+    prsr.add_argument('-m', '--move', action='store_true',
+                      help='Instead of copying move the files')
+    prsr.add_argument('-v', '--verbose', action='store_true',
+                      help='Print more information')
+    prsr.add_argument('-d', '--dir', action='store_true',
+                      help='Maintain directory structure YYYY/MM/DD/file.JPG')
     args = prsr.parse_args()
 
     arg_search = pathlib.Path(args.search)
     arg_target = pathlib.Path(args.target)
     main(arg_search, arg_target,
+         move=args.move,
+         date_dirs=args.dir,
          recursive=args.recursive,
          print_progress=args.progress,
          verbose=args.verbose)
